148b8e55d535ecb6d87953d70876e66504d5d24a
nimeshkverma <nimesh.aug11@gmail.com>
2015-03-24 15:00:01 +0530

    First 2 exercises

---
+++ ex1.py
@@ -0,0 +1,4 @@
+print "Hello World"
+print "Hello Again" + "Hello Thrice"
+print "Trying this the first time" #kidding not the first time
+print "Bye"
---
+++ ex2.py
@@ -0,0 +1,3 @@
+print "Aprogram to demonstrate the use of comments"
+print "As common in many languages '#' is used for commenting" #text after hash is ignored
+print "Hash, Octothorpe, pound etc are all names of '#'"
